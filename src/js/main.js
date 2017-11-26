e362db5b88290a8c2d4b7aa82084749455ff0b3e
Vadym Holovin <holyvad@gmail.com>
2017-11-26 20:10:33 +0300

    Added settings for forms

--- src/js/main.js
+++ src/js/main.js
@@ -16,17 +16,46 @@ $(document).ready(function(){
   $(".bestseller-slider").owlCarousel({
     margin: 1,
     items: 1,
-    dots: false,
-    nav: true,
+    // dots: false,
+    // nav: true,
     navText: ["&#60; Предыдущая сумка", "Следующая сумка &#62;"],
+    responsiveClass:true,
+    responsive:{
+      0:{
+        dots: true,
+        nav: false
+      },
+      650:{
+        dots: false,
+        nav: true
+      }
+    }
   });
 
   $(".bags-slider").owlCarousel({
     margin: 1,
     items: 1,
-    dots: false,
-    nav: true,
+    // dots: false,
+    // nav: true,
     navText: ["&#60; Предыдущая сумка", "Следующая сумка &#62;"],
+    responsiveClass:true,
+    responsive:{
+      0:{
+        dots: true,
+        nav: false
+      },
+      620:{
+        dots: false,
+        nav: true
+      }
+    }
   });
+
+  $(".to-form").mPageScroll2id();
+
+  $(document).on('click', '.modal_btn', function(){
+    $('#small-modal').arcticmodal();
+  });
+
 });
 
